d072523cbb9e47a81ef80ce3e56cfdd224f453ce
BaakhOfficial <baakhofficial@gmail.com>
2023-12-01 01:00:33 +0100

    0.0.4
    Reworked some functions, and added some parameters
    
    Understood why testing is needed. Will write them a little bit later

--- berrylib/wb_shop.py
+++ berrylib/wb_shop.py
@@ -1,6 +1,7 @@
 import datetime
 import requests
 import json
+import pandas as pd
 
 class WbShop:
     """
@@ -12,27 +13,64 @@ class WbShop:
 
     def __str__(self):
          return f"{self.name}"
+    
+    def get_last_week(self):
+        """
+        Returns 2 values of the last week. Monday and Sunday
+        """
+        days_until_sunday = (datetime.date.today().weekday() - 6) % 7
+        last_sunday = datetime.date.today() - datetime.timedelta(days = days_until_sunday)
+        monday = last_sunday - datetime.timedelta(days = 6)
+        return monday, last_sunday
+    
+    def get_request(self,url,js_params, d_type = 'origin'):
+        """
+        Allows you to make a manual request. Returns requests.models.Response in origin format
+        """
+        # !!!!!!!!!!!!!!!!!!!!! Write support for mistakes and different codes !!!!!!!!!!!!!!!!!!! 
+        # 
+        # Authorization parameters
+        headers = {
+            'Authorization': self.api}
+        # request
+        response = requests.get(url,
+                                headers=headers,
+                                params=js_params)
+        # Choosing type of exported data
+        if d_type == 'json':
+            jdata = json.loads(response.text)
+            return jdata
+        elif d_type == 'text':
+            return response.text
+        elif d_type == 'origin':
+            return response
+        elif d_type == 'df':
+             return pd.DataFrame(json.loads(response.text))
+             
 
     def rsr(self, date_from = datetime.date(1997,8,10), date_to = datetime.date(1997,8,10), data_type = 'json'):
         """
         Returns realization sales report
         Default timespan is from the last monday to the last sunday
         dateFrom and dateTo has to be a datetime.datetime objects
+        :data_type: can be "json" "text" "origin" "df" "df_grouped"
         """
+        # !!!!!!!!!!!!!!!!!!!! Rework df_grouped and add df_grouped_rus data type !!!!!!!!!!!!!!!!!!!!!!!!!
         if date_from == datetime.date(1997,8,10) and date_to == datetime.date(1997,8,10):
-            days_until_sunday = (datetime.date.today().weekday() - 6) % 7
-            last_sunday = datetime.date.today() - datetime.timedelta(days = days_until_sunday)
-            monday = last_sunday - datetime.timedelta(days = 6)
-            date_from = monday
-            date_to = last_sunday
+            date_from, date_to = self.get_last_week()
         
-        # Authorization parameters
-        headers = {
-            'Authorization': self.api}
-        # request
-        response = requests.get('https://statistics-api.wildberries.ru/api/v1/supplier/reportDetailByPeriod',
-                                headers=headers,
-                                params={"dateFrom":date_from, "dateTo":date_to, 'rrdid':0})
-        if data_type == 'json':
-            jdata = json.loads(response.text)
-            return jdata
+        if data_type == "df_grouped":
+            df_report = self.get_request(url = 'https://statistics-api.wildberries.ru/api/v1/supplier/reportDetailByPeriod',
+                                    js_params={"dateFrom":date_from, "dateTo":date_to, 'rrdid':0},
+                                    d_type = 'df')
+            return df_report.groupby(by=["sa_name",'nm_id',"doc_type_name","supplier_oper_name",])\
+                                                    [['quantity','retail_amount','retail_price_withdisc_rub',
+                                                    'ppvz_for_pay', 'delivery_amount',
+                                                    'return_amount', 'delivery_rub',
+                                                    'penalty', 'additional_payment', ]]\
+                                                    .sum(numeric_only=True)
+            
+        else:
+            return self.get_request(url = 'https://statistics-api.wildberries.ru/api/v1/supplier/reportDetailByPeriod',
+                                    js_params={"dateFrom":date_from, "dateTo":date_to, 'rrdid':0},
+                                    d_type = data_type)
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='berrylib',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.4',
     description='Wildberries api requests library',
-    install_requires=['requests'],
+    install_requires=['requests', 'pandas'],
     author='Nikolai Baakh',
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
